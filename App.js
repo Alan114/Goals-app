74f1200e759f80a5d99672d48f912a2e68599699
Alan Pecenkovic <alanquad@yahoo.com>
2022-01-18 16:39:02 +0100

    added Modal component in GoalInput

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  ScrollView,
-  FlatList,
-} from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import GoalInput from "./components/GoalInput";
 import GoalItem from "./components/GoalItem";
 
